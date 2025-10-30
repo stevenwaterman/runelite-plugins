d1384e725ce15b7b965ba4cd57726e6ad3ee0e68
mc <neggie333@gmail.com>
2025-10-30 23:08:44 +1100

    reworked as much as i can, might need to wait on adam to expand api

--- src/main/java/com/stevenwaterman/blindfold/BlindfoldPlugin.java
+++ src/main/java/com/stevenwaterman/blindfold/BlindfoldPlugin.java
@@ -42,13 +42,14 @@ import net.runelite.api.Projection;
 import net.runelite.api.Renderable;
 import net.runelite.api.RuneLiteObject;
 import net.runelite.api.Scene;
-import net.runelite.api.SceneTileModel;
-import net.runelite.api.SceneTilePaint;
-import net.runelite.api.Texture;
+import net.runelite.api.Tile;
 import net.runelite.api.TileItem;
+import net.runelite.api.TileObject;
 import net.runelite.api.events.FocusChanged;
 import net.runelite.api.hooks.DrawCallbacks;
 import net.runelite.client.callback.ClientThread;
+import net.runelite.client.callback.RenderCallback;
+import net.runelite.client.callback.RenderCallbackManager;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.events.ConfigChanged;
@@ -64,7 +65,7 @@ import net.runelite.client.ui.overlay.OverlayManager;
 	tags = {"blindfold", "blind", "black", "greenscreen", "render"}
 )
 @Slf4j
-public class BlindfoldPlugin extends Plugin implements DrawCallbacks
+public class BlindfoldPlugin extends Plugin
 {
 	@Inject
 	private Client client;
@@ -81,38 +82,98 @@ public class BlindfoldPlugin extends Plugin implements DrawCallbacks
 	@Inject
 	private BlindfoldOverlay overlay;
 
+	@Inject
+	private RenderCallbackManager renderCallbackManager;
+
 	@Provides
 	BlindfoldPluginConfig provideConfig(ConfigManager configManager)
 	{
 		return configManager.getConfig(BlindfoldPluginConfig.class);
 	}
 
-	private DrawCallbacks interceptedDrawCallbacks;
-	private final HashSet<Plugin> interceptedPlugins = new HashSet<>();
+	private final RenderCallback DISABLE_RENDERING = new RenderCallback(){
+		@Override
+		public boolean drawEntity(Renderable renderable, boolean ui)
+		{
+			return false;
+		}
 
-	private final DisableRenderCallbacks DISABLE_RENDERING = new DisableRenderCallbacks();
+		@Override
+		public boolean drawTile(Scene scene, Tile tile)
+		{
+			return false;
+		}
+
+		@Override
+		public boolean drawObject(Scene scene, TileObject object)
+		{
+			return false;
+		}
+	};
+
+	private final RenderCallback rcb = new RenderCallback()
+	{
+		@Override
+		public boolean drawEntity(Renderable renderable, boolean ui)
+		{
+			boolean isRuneLiteObject = renderable instanceof RuneLiteObject;
+			boolean render =
+				renderable == client.getLocalPlayer() ||
+				config.enableScenery() && (
+					renderable instanceof Model ||
+					renderable instanceof ModelData ||
+					(!isRuneLiteObject && renderable instanceof GraphicsObject) ||
+					renderable instanceof DynamicObject
+				) ||
+				config.enableEntities() && (
+					renderable instanceof Projectile ||
+					renderable instanceof TileItem ||
+					renderable instanceof Actor
+				) ||
+				isRuneLiteObject && config.enableRuneLiteObjects();
+			// if (!render) check clickbox
+			return render;
+		}
+
+		@Override
+		public boolean drawTile(Scene scene, Tile tile)
+		{
+			return config.enableTerrain();
+		}
+
+		@Override
+		public boolean drawObject(Scene scene, TileObject object)
+		{
+//			if (!render) check clickbox
+			return config.enableScenery();
+		}
+	};
 
 	@Override
 	protected void startUp()
 	{
 		overlayManager.add(overlay);
-		clientThread.invokeLater(() -> {
-			// We want to specifically ignore the return value here, so RuneLite doesn't
-			// interpret this as something that needs to be retried until it returns true
-			interceptDrawCalls();
-		});
+		clientThread.invokeLater(() ->
+			{
+				renderCallbackManager.register(rcb);
+
+				if (client.getGameState() == GameState.LOGGED_IN)
+					client.setGameState(GameState.LOADING);
+			}
+		);
 	}
 
 	@Override
 	protected void shutDown()
 	{
 		overlayManager.remove(overlay);
-		clientThread.invokeLater(() -> {
-			if (client.getDrawCallbacks() == this || client.getDrawCallbacks() == DISABLE_RENDERING)
-				client.setDrawCallbacks(interceptedDrawCallbacks);
-			interceptedDrawCallbacks = null;
-			interceptedPlugins.clear();
-		});
+
+		clientThread.invoke(() ->
+			{
+				renderCallbackManager.unregister(rcb);
+//				renderCallbackManager.unregister(DISABLE_RENDERING);
+			}
+		);
 	}
 
 //	@Subscribe
@@ -122,217 +183,102 @@ public class BlindfoldPlugin extends Plugin implements DrawCallbacks
 //		}
 //	}
 
-	@Subscribe
-	public void onPluginChanged(PluginChanged pluginChanged)
-	{
-		var plugin = pluginChanged.getPlugin();
-		if (plugin == this)
-			return;
-
-		// After another plugin has started or stopped,
-		// check whether draw calls need to be intercepted.
-		// We assume that other plugins are well-behaved,
-		// and that they only replace draw calls once each.
-		clientThread.invokeLater(() -> {
-			if (pluginChanged.isLoaded())
-			{
-				// This should never happen, but is probably worth ensuring
-				if (interceptedPlugins.contains(plugin))
-					return;
-
-				if (interceptDrawCalls())
-					interceptedPlugins.add(plugin);
-			}
-			else if (client.getDrawCallbacks() == null)
-			{
-				// Draw callbacks have been reset.
-				// Regardless of whether the plugin was previously intercepted,
-				// the chain is broken, and we should just reset everything
-				interceptedDrawCallbacks = null;
-				interceptedPlugins.clear();
-			}
-			else
-			{
-				// Assume the other plugin is well-behaved and will restore
-				// the draw callbacks to our prior intercepted state
-				interceptedPlugins.remove(plugin);
-			}
-		});
-	}
-
-	private boolean interceptDrawCalls()
-	{
-		assert client.isClientThread();
-
-		// Don't attempt to intercept draw calls during loading, since switching config profiles
-		// may lead to multiple plugins modifying draw callbacks rapidly
-		if (client.getGameState() == GameState.LOADING)
-			return false;
-
-		// We only care about replacing draw callbacks while a GPU plugin is active
-		if (!client.isGpu())
-			return false;
-
-		var drawCallbacks = client.getDrawCallbacks();
-		if (drawCallbacks == null)
-			return false;
-
-		// Check if draw calls have already been intercepted, and are unchanged
-		if (drawCallbacks == this || drawCallbacks == DISABLE_RENDERING)
-			return false;
-
-		interceptedDrawCallbacks = drawCallbacks;
-		client.setDrawCallbacks(this);
-		return true;
-	}
-
-	@Override
-	public void drawScene(double cameraX, double cameraY, double cameraZ, double cameraPitch, double cameraYaw, int plane)
-	{
-		if (interceptedDrawCallbacks != null)
-			interceptedDrawCallbacks.drawScene(cameraX, cameraY, cameraZ, cameraPitch, cameraYaw, plane);
-	}
-
-	@Override
-	public void postDrawScene() {
-		if (interceptedDrawCallbacks != null)
-			interceptedDrawCallbacks.postDrawScene();
-	}
-
-	@Override
-	public void animate(Texture texture, int diff)
-	{
-		// Probably a no-op, but pass it on anyway
-		if (interceptedDrawCallbacks != null)
-			interceptedDrawCallbacks.animate(texture, diff);
-	}
-
-	@Override
-	public boolean tileInFrustum(Scene scene, float pitchSin, float pitchCos, float yawSin, float yawCos, int cameraX, int cameraY, int cameraZ, int plane, int msx, int msy)
-	{
-		if (interceptedDrawCallbacks != null)
-			interceptedDrawCallbacks.tileInFrustum(scene, pitchSin, pitchCos, yawSin, yawCos, cameraX, cameraY, cameraZ, plane, msx, msy);
-		return true;
-	}
-
-	@Subscribe
-	public void onFocusChanged(FocusChanged event)
-	{
-		if (client.getGameState() == GameState.LOGGED_IN && config.disableRendering() && !event.isFocused()){
-			clientThread.invoke(() -> client.setDrawCallbacks(DISABLE_RENDERING));
-			log.debug("Focus changed: rendering disabled");
-		}
-		else
-		{
-			if (client.getDrawCallbacks() == DISABLE_RENDERING)
-			{
-				clientThread.invoke(() -> client.setDrawCallbacks(this));
-				log.debug("Focus changed: rendering reenabled");
-			}
-		}
-	}
+//	@Subscribe
+//	public void onFocusChanged(FocusChanged event)
+//	{
+//		if (client.getGameState() == GameState.LOGGED_IN && config.disableRendering() && !event.isFocused()){
+//			clientThread.invoke(() ->
+//				{
+//					renderCallbackManager.unregister(rcb);
+//					renderCallbackManager.register(DISABLE_RENDERING);
+//					client.setGameState(GameState.LOADING);
+//				}
+//			);
+//			log.debug("Focus changed: rendering disabled");
+//		}
+//		else
+//		{
+//			clientThread.invoke(() ->
+//				{
+//					renderCallbackManager.register(rcb);
+//					renderCallbackManager.unregister(DISABLE_RENDERING);
+//					client.setGameState(GameState.LOADING);
+//				}
+//			);
+//			log.debug("Focus changed: rendering reenabled");
+//		}
+//	}
 
 	@Subscribe
 	public void onNotificationFired(NotificationFired event){
-		if (client.getDrawCallbacks() == DISABLE_RENDERING){
-			clientThread.invoke(() -> client.setDrawCallbacks(this));
-			log.debug("notification sent: rendering reenabled");
-		}
+		clientThread.invoke(() ->
+			{
+				renderCallbackManager.register(rcb);
+//				renderCallbackManager.unregister(DISABLE_RENDERING);
+				client.setGameState(GameState.LOADING);
+			}
+		);
+		log.debug("notification sent: rendering reenabled");
 	}
 
 	@Subscribe
 	public void onConfigChanged(ConfigChanged event){
-		if (!Objects.equals(event.getGroup(), config.GROUP)){
+		if (!Objects.equals(event.getGroup(), BlindfoldPluginConfig.GROUP)){
 			return;
 		}
 		if (Objects.equals(event.getKey(), "disableRendering")){
-			if (Objects.equals(event.getNewValue(), "false") && client.getDrawCallbacks() == DISABLE_RENDERING){
-				clientThread.invoke(() -> client.setDrawCallbacks(this));
+			if (Objects.equals(event.getNewValue(), "false")){
+				clientThread.invoke(() ->
+					{
+						renderCallbackManager.register(rcb);
+//						renderCallbackManager.unregister(DISABLE_RENDERING);
+						client.setGameState(GameState.LOADING);
+					}
+				);
 			}
+			else {
+				clientThread.invoke(() ->
+					{
+						renderCallbackManager.unregister(rcb);
+//						renderCallbackManager.register(DISABLE_RENDERING);
+						client.setGameState(GameState.LOADING);
+					}
+				);
+			}
+		}
+
+		if (Objects.equals(event.getKey(), "enableTerrain") || Objects.equals(event.getKey(), "enableScenery"))
+		{
+			clientThread.invokeLater(() -> {
+				if (client.getGameState() == GameState.LOGGED_IN)
+					client.setGameState(GameState.LOADING);
+			});
 		}
 	}
 
-	@Override
-	public void loadScene(Scene scene) {
-		if (interceptedDrawCallbacks != null)
-			interceptedDrawCallbacks.loadScene(scene);
-	}
-
-	@Override
-	public void swapScene(Scene scene) {
-		if (interceptedDrawCallbacks != null)
-			interceptedDrawCallbacks.swapScene(scene);
-	}
-
-	@Override
-	public void drawScenePaint(Scene scene, SceneTilePaint paint, int plane, int tileX, int tileY) {
-		if (interceptedDrawCallbacks != null && config.enableTerrain())
-			interceptedDrawCallbacks.drawScenePaint(scene, paint, plane, tileX, tileY);
-	}
-
-	@Override
-	public void drawSceneTileModel(Scene scene, SceneTileModel model, int tileX, int tileY)	{
-		if (interceptedDrawCallbacks != null && config.enableTerrain())
-			interceptedDrawCallbacks.drawSceneTileModel(scene, model, tileX, tileY);
-	}
-
-	@Override
-	public void draw(int overlayColor)
+	// Check the clickbox even if not drawn
+	public void checkClickbox(Projection projection, Scene scene, Renderable renderable, int orientation, int x, int y, int z, long hash)
 	{
-		// Draw the frame normally. UI-hiding is handled in BlindfoldOverlay
-		if (interceptedDrawCallbacks != null)
-			interceptedDrawCallbacks.draw(overlayColor);
-	}
-
-	@Override
-	public void draw(Projection projection, Scene scene, Renderable renderable, int orientation, int x, int y, int z, long hash)
-	{
-		if (interceptedDrawCallbacks == null)
+		Model model = renderable instanceof Model ? (Model) renderable : renderable.getModel();
+		if (model == null)
 			return;
 
-		boolean isRuneLiteObject = renderable instanceof RuneLiteObject;
-		boolean render =
-			renderable == client.getLocalPlayer() ||
-			config.enableScenery() && (
-				renderable instanceof Model ||
-				renderable instanceof ModelData ||
-				(!isRuneLiteObject && renderable instanceof GraphicsObject) ||
-				renderable instanceof DynamicObject
-			) ||
-			config.enableEntities() && (
-				renderable instanceof Projectile ||
-				renderable instanceof TileItem ||
-				renderable instanceof Actor
-			) ||
-			isRuneLiteObject && config.enableRuneLiteObjects();
+		// Apply height to renderable from the model
+		if (model != renderable)
+			renderable.setModelHeight(model.getModelHeight());
 
-		// skip rendering, but process clickbox if render == false
+		model.calculateBoundsCylinder();
 
-		if (render) {
-			interceptedDrawCallbacks.draw(projection, scene, renderable, orientation, x, y, z, hash);
-		} else {
-			// Check the clickbox even if not drawn
-			Model model = renderable instanceof Model ? (Model) renderable : renderable.getModel();
-			if (model == null)
-				return;
-
-			// Apply height to renderable from the model
-			if (model != renderable)
-				renderable.setModelHeight(model.getModelHeight());
-
-			model.calculateBoundsCylinder();
-
-			if (projection instanceof IntProjection)
+		if (projection instanceof IntProjection)
+		{
+			IntProjection p = (IntProjection) projection;
+			if (!isVisible(model, p.getPitchSin(), p.getPitchCos(), p.getYawSin(), p.getYawCos(), x - p.getCameraX(), y - p.getCameraY(), z - p.getCameraZ()))
 			{
-				IntProjection p = (IntProjection) projection;
-				if (!isVisible(model, p.getPitchSin(), p.getPitchCos(), p.getYawSin(), p.getYawCos(), x - p.getCameraX(), y - p.getCameraY(), z - p.getCameraZ()))
-				{
-					return;
-				}
+				return;
 			}
-
-			client.checkClickbox(projection, model, orientation, x, y, z, hash);
 		}
+
+		client.checkClickbox(projection, model, orientation, x, y, z, hash);
 	}
 
 	/**
